3391902f0588f6967e292333e704170252b52426
Vicky Steeves <vicky.steeves@nyu.edu>
2018-02-21 18:44:29 -0500

    initial commit of theme materials

--- class-materials/txt-crawl-clean.py
+++
@@ -1,8 +0,0 @@
-#import the right Python libraries to complete our requests
-#the library to grab URLs and data from webpages
-import requests
-
-#reads in the URL we want to scrape from
-for line in requests.get('http://bit.ly/2dfjvCC').text.splitlines():
-    #strips away white space from the beginning and end of a string -- can strip out other things if we want
-    print(line.strip())
